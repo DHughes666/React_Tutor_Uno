21812d5785250b19492e3e22a4fce486f2bec804
Des Hughes <88185619+DHughes666@users.noreply.github.com>
2023-08-17 09:18:17 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import List from "./list.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
